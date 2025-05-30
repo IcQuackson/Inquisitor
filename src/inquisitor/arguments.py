d8640484dd683f467efcb4665f43c5936b1977bb
IcQuackson <pedrorenato1998@gmail.com>
2025-05-30 19:47:03 +0100

    feat: add arg ip and mac validation

--- src/inquisitor/arguments.py
+++ src/inquisitor/arguments.py
@@ -1,4 +1,7 @@
 import sys
+import re
+import ipaddress
+from ipaddress import AddressValueError
 
 def print_usage(error_msg):
 	if error_msg:
@@ -13,18 +16,37 @@ def print_usage(error_msg):
 	print()
 	print("Example:")
 	print("  python3 poison.py 172.16.238.30 02:aa:bb:cc:dd:10 172.16.238.20 02:42:ac:00:00:bb")
-
 	
 
 def arg_count_valid(args):
 	return len(args) == 4
 
-def validate_arguments():
-	args = sys.argv[1:]
+def mac_is_valid(mac):
+	return re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", mac.lower())
 
-	if (not arg_count_valid(args)):
+def ip_is_valid(ip):
+	try:
+		ipaddress.IPv4Address(ip)
+		return True
+	except AddressValueError as e:
+		print("IPv4 address not valid! " + str(e))
+	return False
+
+
+def validate_arguments(args):
+
+	if not arg_count_valid(args):
 		print_usage("Wrong argument count!")
 		sys.exit()
 
-	args_dict = {}
-	#args_dict["src_mac": arg]
+	arg_labels = ["ip_src", "mac_src", "ip_target", "mac_target"]
+	args = dict(zip(arg_labels, args))
+
+	if not (mac_is_valid(args["mac_src"]) and mac_is_valid(args["mac_target"])):
+		print("MAC address not valid!")
+		sys.exit()
+	
+	if not (ip_is_valid(args["ip_src"]) and ip_is_valid(args["ip_target"])):
+		sys.exit()
+
+	print(args)
